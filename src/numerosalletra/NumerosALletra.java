84d60234361054d2397d45f9a5a82f0ded92adb6
Pau Asensi <pauasensitalens@gmail.com>
2020-12-15 21:26:33 +0100

    Activitat en leas funcions fetes

--- src/numerosalletra/NumerosALletra.java
+++ src/numerosalletra/NumerosALletra.java
@@ -1,21 +1,128 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package numerosalletra;
 
 /**
  *
- * @author daw14
+ * @author pau
  */
 public class NumerosALletra {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
     }
+    
+    /**
+     * 
+     * @param q Numero del que volem saber els digits que te.
+     * @return Retorna el numero de digits que te : 1 , 2 o 3 ....
+     */
+    public static int digitUnitats(int q){
+        if(q<10)
+            return 1;
+        
+        return 1+digitUnitats(q/10);
+    }
+    
+    
+    public static int digitDesenes(int q){
+       q = q /10;
+       return q%10;
+        
+    }
+    
+    public static int digitCentenes(int q){
+        
+        q = q /100;
+       return q%100;
+    }
+    
+    public static  String digitEnLLetres(int dig){
+        
+        switch(dig){
+            case 1:
+		return "uno";
+            case 2:
+		return "dos";
+            case 3:
+		return "tres";
+            case 4:
+		return "cuatre";
+            case 5:
+		return "cinc";
+            case 6:
+		return "sis";
+            case 7:
+		return "set";
+            case 8:
+		return "huit";
+            case 9:
+		return "nou";
+            
+            default:
+		return "";
+            
+        }
+    }
+    
+    
+    public static String desenaEnLletres(int des){
+        
+        switch(des){
+            case 1:
+		return "deu";
+            case 2:
+		return "vint";
+            case 3:
+		return "trenta";
+            case 4:
+		return "quaranta";
+            case 5:
+		return "cincuanta";
+            case 6:
+		return "sisanta";
+            case 7:
+		return "setanta";
+            case 8:
+		return "huitanta";
+            case 9:
+		return "noranta";
+            
+            default:
+		return "";
+            
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
